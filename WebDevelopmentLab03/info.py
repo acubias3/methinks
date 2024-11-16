503bd0535eb192550f7dc2252d2262a953bb583a
acubias3 <ashley.c.100204@gmail.com>
2024-11-16 15:16:04 -0500

    Update info.py

--- WebDevelopmentLab03/info.py
+++ WebDevelopmentLab03/info.py
@@ -7,7 +7,7 @@ about_me = "I'm Hermione Granger. Current Minister for Magic and former Hogwarts
 
 
 #CHANGE BELOW (OPTIONAL)
-linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+linkedin_image_url = "https://www.linkedin.com/in/hermionegranger/"
 github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
 email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
 
